e81d39b180aa027f39be0c4de7939dbcc8304a54
Alex Trapholov <trapholov@gmail.com>
2015-10-02 17:04:54 +0300

    Some logic improvements

--- jquery.initialize.js
+++ jquery.initialize.js
@@ -2,98 +2,79 @@
 
 ;(function($) {
 
-	$.fn.initialize = function(init, firstInit) {
-		var $t = this; //reference for deeper functions
-		var selector = $t.selector; //get called selector for inits in future
+    $.fn.initialize = function(init, firstInit) {
+        var $t = this; //reference for deeper functions
+        var selector = $t.selector; //get called selector for inits in future
 
-		if ( typeof init !== "function" || !selector ) return $($t); //if no initialization function, no sense to continue
+        if ( typeof init !== "function" || !selector ) return $($t); //if no initialization function, no sense to continue
 
-		// select the target node to observe
-		var target = $('body')[0];
+        // select the target node to observe
+        var target = $('body')[0];
 
-		$t.firstInitsCalled = $t.firstInitsCalled || []; //we need to collect info about firstInits that can be called only once
+        $t.firstInitsCalled = $t.firstInitsCalled || []; //we need to collect info about firstInits that can be called only once
 
-		//if proper firstInit and was never called before
-		if ( typeof firstInit == "function" && $t.firstInitsCalled.indexOf(firstInit) == -1 ) {
-			$t.firstInitsCalled.push(firstInit); //add to called list
-			firstInit(); //call
-		}
+        //if proper firstInit and was never called before
+        if ( typeof firstInit == "function" && $t.firstInitsCalled.indexOf(firstInit) == -1 ) {
+            $t.firstInitsCalled.push(firstInit); //add to called list
+            firstInit(); //call
+        }
 
+        $t.initData = $t.initData || {}; //we will collect inits for selectors here
+        $t.initData[selector] = $t.initData[selector] || []; //if no inits yet for this selector, add empty array
+        $t.initData[selector].push(init); //and add given init
 
-		$t.initData = $t.initData || {}; //we will collect inits for selectors here
-		$t.initData[selector] = $t.initData[selector] || []; //if no inits yet for this selector, add empty array
-		$t.initData[selector].push(init); //and add given init
+        //only once initialize observer
+        if ( !this.initializedObserver ) {
+            this.initializedObserver = true; //dont initialize again
 
+            window.MutationObserver = window.MutationObserver || window.WebKitMutationObserver; //unify mutation obj
 
-		//firstly normally call it on given set as .each do, but add init function to each element called inits list
-		$(this).each(function(){
-			this.initsCalled = this.initsCalled || []; //if no list, add empty arrat
-			if ( this.initsCalled.indexOf(init) == -1 ) { //if havent been called before
-				this.initsCalled.push(init); //add to called list
-				$(this).each(init); //call
-			}
-		});
+            // create an observer instance
+            var observer = new MutationObserver(function(mutations) {
+                //foreach mutation
+                $.each(mutations , function(i, mutation) {
+                    //lets get mutation target basing of mutation type
+                    var target = $();
+                    //if child list, lets add all addedNodes
+                    if ( mutation.type == "childList" && mutation.addedNodes.length ) {
+                        $.each(mutation.addedNodes, function(i, addedNode){
+                            if ($(addedNode).is(selector)){
+                                target = target.add(addedNode);
+                            }
+                        });
+                    }
 
+                    //for each watched selector
+                    for ( selector in $t.initData ) {
+                        var inits = $t.initData[selector]; //get functions that this selector has to initialize
 
-		//only once initialize observer
-		if ( !this.initializedObserver ) {
-			this.initializedObserver = true; //dont initialize again
+                        //for each item that match selector and is in mutated area
+                        target.each(function(){
+                            var toInitSingle = this;
+                            //create list of called inits if no list yet
+                            toInitSingle.initsCalled = toInitSingle.initsCalled || [];
 
-			window.MutationObserver = window.MutationObserver || window.WebKitMutationObserver; //unify mutation obj
+                            //foreach function for this selector
+                            $.each(inits, function(i, init){
+                                //if it's not yet called on this element
+                                if ( toInitSingle.initsCalled.indexOf(init) == -1 ) {
+                                    toInitSingle.initsCalled.push(init); //add it to called list
+                                    $(toInitSingle).each(init); //initialize it
+                                }
+                            });
+                        });
+                    }
+                });
+            });
 
-			// create an observer instance
-			var observer = new MutationObserver(function(mutations) {
-				//foreach mutation
-				$.each(mutations , function(i, mutation) {
-					//lets get mutation target basing of mutation type
-					var target = $();
-					if ( mutation.type == "attributes" ) target = $(mutation.target); //if attr changed, single target always
-					//if child list, lets add all addedNodes
-					if ( mutation.type == "childList" && mutation.addedNodes.length ) {
-						$.each(mutation.addedNodes, function(i, addedNode){
-							target = target.add(addedNode);
-						});
-					}
+            // configuration of the observer to be sure we dont miss possible way of adding wanted element
+            var config = { attributes: true, childList: true, characterData: false, subtree : true };
 
-					//for each watched selector
-					for ( selector in $t.initData ) {
-						var inits = $t.initData[selector]; //get functions that this selector has to initialize
+            //start the observer
+            observer.observe(target, config);
+        }
 
-						//check children of elem if they match current selector
-						var toInit = target.find(selector);
-						//also check item itself, if it's good, add to set
-						if ( target.is(selector) ) toInit = toInit.add(target);
-
-						//for each item that match selector and is in mutated area
-						toInit.each(function(){
-							
-							var toInitSingle = this;
-							//create list of called inits if no list yet
-							toInitSingle.initsCalled = toInitSingle.initsCalled || [];
-
-							//foreach function for this selector
-							$.each(inits, function(i, init){
-								//if it's not yet called on this element
-								if ( toInitSingle.initsCalled.indexOf(init) == -1 ) {
-									toInitSingle.initsCalled.push(init); //add it to called list
-									$(toInitSingle).each(init); //initialize it
-								}
-							});
-						});
-					}
-				});    
-			});
-			 
-			// configuration of the observer to be sure we dont miss possible way of adding wanted element 
-			var config = { attributes: true, childList: true, characterData: false, subtree : true };
-			 
-			//start the observer
-			observer.observe(target, config);
-		}
-
-		return $(this);
-	}
-
-
+        return $(this);
+    }
 
 })(jQuery);
